91bbc3de6d10539c11fbb5508394f7c205288ffa
tjrdbfl <tjrdbfl123@gmail.com>
2024-08-14 15:34:49 +0900

    Initial commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,31 +11,31 @@ export function middleware(request: NextRequest) {
   const payment = cookieStore.get('payment')?.value;
   const pathname = request.nextUrl.pathname;
 
-  // //1. 로그인된 사용자에 대한 redirection
-  // if (accessToken && (pathname === '/login' || pathname === '/register')) {
-  //   return NextResponse.redirect(new URL('/', request.url));
-  // }
+  //1. 로그인된 사용자에 대한 redirection
+  if (accessToken && (pathname === '/login' || pathname === '/register')) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
   
-  // //2. 접근 권한이 없는 사용자에 대한 redirection
-  // if ((accessToken === undefined && refreshToken===undefined)
-  //   && (pathname.match(/^\/exam\/\d+/)
-  //     || pathname.match(/^\/level\/\d+/)
-  //     || pathname.match(/^\/part\/\d+/)
-  //     || pathname.startsWith('/level-test/test')
-  //     || pathname.startsWith('/my-page'))
-  // ) {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
+  //2. 접근 권한이 없는 사용자에 대한 redirection
+  if ((accessToken === undefined && refreshToken===undefined)
+    && (pathname.match(/^\/exam\/\d+/)
+      || pathname.match(/^\/level\/\d+/)
+      || pathname.match(/^\/part\/\d+/)
+      || pathname.startsWith('/level-test/test')
+      || pathname.startsWith('/my-page'))
+  ) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
-  // //3. 역할에 따른 권한 부여
-  // if (pathname.startsWith('/admin') && userRole !== 'ROLE_ADMIN') {
-  //   return NextResponse.redirect(new URL('/', request.url));
-  // }
+  //3. 역할에 따른 권한 부여
+  if (pathname.startsWith('/admin') && userRole !== 'ROLE_ADMIN') {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
 
-  // //4. 사용자에게 보여주지 않을 페이지 redirection
-  // if(pathname==='/login/callback'){
-  //   return NextResponse.redirect(new URL('/login',request.url));
-  // }
+  //4. 사용자에게 보여주지 않을 페이지 redirection
+  if(pathname==='/login/callback'){
+    return NextResponse.redirect(new URL('/login',request.url));
+  }
 
   // //5. 사용자 결제 여부에 따른 유료 상품 접근 권한 제어
   // // if(payment==='false'
